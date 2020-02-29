acfd6fcb6c9af8d05dbaa07b0ae0a4477d89877c
snowzhop <snowzhop@gmail.com>
2020-02-29 15:24:42 +0300

    Added new window for letter showing

--- LetterDialog/letterdialog.cpp
+++ LetterDialog/letterdialog.cpp
@@ -1,5 +1,77 @@
 #include "letterdialog.h"
 
-LetterDialog::LetterDialog() {
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QWebEngineView>
+#include <QStringRef>
 
+#include <cstring>
+
+LetterDialog::LetterDialog(QWidget* parent) :
+    QMainWindow (parent),
+    dateLine(new QLineEdit(this)),
+    fromLine(new QLineEdit(this)),
+    webView(new QWebEngineView) {
+
+    QWidget* centralWidget = new QWidget(this);
+    this->setCentralWidget(centralWidget);
+
+    QLabel* dateLabel   = new QLabel(QString("Дата:"), this);
+    QLabel* fromLabel   = new QLabel(QString("От кого:"), this);
+//    QLabel* letterLabel = new QLabel("Letter:", this);
+
+    dateLine->setReadOnly(true);
+    fromLine->setReadOnly(true);
+
+    QVBoxLayout* mainLayout     = new QVBoxLayout(centralWidget);
+    QVBoxLayout* dataLayout     = new QVBoxLayout();
+    QHBoxLayout* dateLayout     = new QHBoxLayout();
+    QHBoxLayout* fromLayout     = new QHBoxLayout();
+//    QVBoxLayout* letterLayout   = new QVBoxLayout();
+
+    dateLayout->addWidget(dateLabel);
+    dateLayout->addWidget(dateLine);
+
+    fromLayout->addWidget(fromLabel);
+    fromLayout->addWidget(fromLine);
+
+//    letterLayout->addWidget(letterLabel);
+
+    dataLayout->addLayout(fromLayout);
+    dataLayout->addLayout(dateLayout);
+//    dataLayout->addLayout(letterLayout);
+    dataLayout->setSizeConstraint(QLayout::SetFixedSize);
+
+    mainLayout->addLayout(dataLayout);
+    mainLayout->addWidget(webView);
+
+    webView->setHtml("Хаюшки, мазафака");
+}
+
+LetterDialog::~LetterDialog() {
+
+}
+
+void LetterDialog::showLetter(POP3Client& pop3Client, const int& letterNumber) {
+    this->show();
+    char* request = pop3Client.sendRequest(std::string("RETR ").
+                                           append(std::to_string(letterNumber)).
+                                           append("\r\n"));
+
+    std::string_view strRequest(request);
+    delete[] request;
+
+    auto dateBeginPos = strRequest.find("Date: ") + 6;
+    auto dateEndPos = strRequest.find("\r\n", dateBeginPos);
+
+    std::string_view dateStr = strRequest.substr(dateBeginPos, dateEndPos - dateBeginPos);
+    dateLine->setText(std::string(dateStr).c_str());
+
+    auto fromBeginPos = strRequest.find("From: ") + 6;
+    auto fromEndPos = strRequest.find("\r\n", fromBeginPos);
+
+    if (strRequest[fromBeginPos] == '=') {
+        qDebug() << "kodirovochka";
+    }
 }
--- LetterDialog/letterdialog.h
+++ LetterDialog/letterdialog.h
@@ -1,11 +1,26 @@
 #ifndef LETTERDIALOG_H
 #define LETTERDIALOG_H
 
+#include "POP3/pop3Client.h"
 
-class LetterDialog
-{
+#include <QDialog>
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QWebEngineView>
+
+class LetterDialog : public QMainWindow {
+    Q_OBJECT
 public:
-    LetterDialog();
+    LetterDialog(QWidget* parent = nullptr);
+    virtual ~LetterDialog();
+
+    void showLetter(POP3Client& pop3Client, const int& letterNumber);
+
+private:
+    QLineEdit* dateLine = nullptr;
+    QLineEdit* fromLine = nullptr;
+
+    QWebEngineView* webView = nullptr;
 };
 
 #endif // LETTERDIALOG_H
